315a8b69bb36088d8c56d7219b9bcaa3fba69380
NotoriousPlatinum <54433490+dante-effekt@users.noreply.github.com>
2021-02-04 22:50:45 -0600

    Corrección, no es forzoso ingresar más autores.
    Anteriormente, si un autor utiliza el método crearArticulo, era obligatorio que ingresara al menos otro autor, ya no.

--- proyecto3/src/Peticiones/peticionesArticulos/pedirAutores.java
+++ proyecto3/src/Peticiones/peticionesArticulos/pedirAutores.java
@@ -27,53 +27,74 @@ public class pedirAutores extends PeticionesArticulo {
         Scanner sc = new Scanner(System.in);  
         int numEmpleado; //Numero de empleado - autor
         int op=2;
-            
-        if(datosGenerales.getSetDeAutores().isEmpty()){
-            System.out.println("No existen autores creados");
-            System.out.println("... no se pueden asignar autores a un Artículo");
+                
+        System.out.println("");            
+        System.out.println("Desea ingresar un nuevo autor además de usted?, ingrese 1");
+        while(true){
+            try{
+                op = sc.nextInt();
+
+            }catch(InputMismatchException im){
+                System.out.println("");
+                System.out.println("Necesita ingresar un dato numérico, intente nuevamente");
+            }catch(IllegalArgumentException ia){
+                System.out.println(" ");
+                System.out.println("Ingrese un numero correctamente, intente nuevamente");
+                continue;
+            }
+            break;
+        }
+        
+        if(op == 1){                                    
+            if(datosGenerales.getSetDeAutores().isEmpty()){
+                System.out.println("No existen autores creados");
+                System.out.println("... no se pueden asignar autores a un Artículo");
+            } else {
+                do{
+                    boolean ok = false;
+
+                    System.out.println("Se asignaran autores a un Articulo");
+                    System.out.println("Nota: Maximo tres autores por articulo");
+
+                    System.out.println("Autores disponibles: ");    
+                    imprimirAutoresSencillo impAS = new imprimirAutoresSencillo();
+                    impAS.realizarImpresion(datosGenerales);
+                    System.out.println("");        
+
+                    do {
+                        System.out.println("Elegir numero de autor : ");
+                        numEmpleado = sc.nextInt();                
+                        if(datosGenerales.existsNumeroEmpleado(numEmpleado)){
+                            objetivo.setAutor((Autor) datosGenerales.buscarNumeroEmpleado(numEmpleado));
+                            System.out.println("Autor asignado correctamente");
+                            System.out.println("");
+                            ok = true;
+                        } else {
+                            System.out.println("El numero de empleado no existe, ingrese uno valido");
+                            System.out.println("");
+                        }
+                    } while (ok == false);
+
+                    System.out.println("");            
+                    System.out.println("Desea ingresar un nuevo autor, ingrese 1");
+                    while(true){
+                        try{
+                            op = sc.nextInt();
+
+                        }catch(InputMismatchException im){
+                            System.out.println("");
+                            System.out.println("Necesita ingresar un dato numérico, intente nuevamente");
+                        }catch(IllegalArgumentException ia){
+                            System.out.println(" ");
+                            System.out.println("Ingrese un numero correctamente, intente nuevamente");
+                            continue;
+                        }
+                        break;
+                    }               
+                }while(op==1);
+            }
         } else {
-            do{
-                boolean ok = false;
-
-                System.out.println("Se asignaran autores a un Articulo");
-                System.out.println("Nota: Maximo tres autores por articulo");
-
-                System.out.println("Autores disponibles: ");    
-                imprimirAutoresSencillo impAS = new imprimirAutoresSencillo();
-                impAS.realizarImpresion(datosGenerales);
-                System.out.println("");        
-
-                do {
-                    System.out.println("Elegir numero de autor : ");
-                    numEmpleado = sc.nextInt();                
-                    if(datosGenerales.existsNumeroEmpleado(numEmpleado)){
-                        objetivo.setAutor((Autor) datosGenerales.buscarNumeroEmpleado(numEmpleado));
-                        System.out.println("Autor asignado correctamente");
-                        System.out.println("");
-                        ok = true;
-                    } else {
-                        System.out.println("El numero de empleado no existe, ingrese uno valido");
-                        System.out.println("");
-                    }
-                } while (ok == false);
-
-                System.out.println("");            
-                System.out.println("Desea ingresar un nuevo autor, ingrese 1");
-                while(true){
-                    try{
-                        op = sc.nextInt();
-
-                    }catch(InputMismatchException im){
-                        System.out.println("");
-                        System.out.println("Necesita ingresar un dato numérico, intente nuevamente");
-                    }catch(IllegalArgumentException ia){
-                        System.out.println(" ");
-                        System.out.println("Ingrese un numero correctamente, intente nuevamente");
-                        continue;
-                    }
-                    break;
-                }               
-            }while(op==1);
+            
         }
     }
 }
